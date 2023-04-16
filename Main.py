0080861b80e3aae9c214797900d0c09fa580bed6
Ahmed Hassan <ad.hsn77@gmail.com>
2023-04-16 23:20:31 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 #Importing Modules:
 import pandas as pd
-from Modules import Data_splitter,Linear_Regression
+from Modules import Data_splitter,Linear_Regression,Polynomial_Regression
 
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', None)
@@ -11,15 +11,19 @@ pd.set_option('display.max_colwidth', 1)
 Movie_Data = pd.read_csv("movies-regression-dataset.csv")
 print(Movie_Data)
 
-X = Movie_Data.iloc[:,0:-1]#Features
-Y = Movie_Data.iloc[:,-1]#Label
-
 #Data Preprocessing:
 """
 THIS SECTION STILL REQUIRED
 """
+
+X = Movie_Data.iloc[:,0:-1]#Features
+Y = Movie_Data.iloc[:,-1]#Label
+
 #Data Splitting:
 X_train, X_test, y_train, y_test =  Data_splitter(X,Y)
 
 #Linear Regression(resulting run time error for now as the data isn't preprocessed yet):
 Linear_Regression(X_train, X_test, y_train, y_test)
+
+#Polynomial Regression:
+Polynomial_Regression(X_train, X_test, y_train, y_test)
