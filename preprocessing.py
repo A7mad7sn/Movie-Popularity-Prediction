f859c50f025c5a97c7e3b3cd82efc418a07feb10
Ahmed Hassan <ad.hsn77@gmail.com>
2023-04-19 18:25:05 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer
+
+def featureScaling(X,a,b):
+    X = np.array(X)
+    Normalized_X=np.zeros((X.shape[0],X.shape[1]))
+    for i in range(X.shape[1]):
+        Normalized_X[:,i]=((X[:,i]-min(X[:,i]))/(max(X[:,i])-min(X[:,i])))*(b-a)+a
+    return Normalized_X
 
 
 def Feature_Encoder(X, cols):
@@ -41,15 +49,18 @@ def Preprocessing(Movie_Data):
     Y = Movie_Data['vote_average']
     Movie_Data = Movie_Data.drop(columns=['vote_average'])
     
+    #handling the missing value in runtime:
+    Movie_Data['runtime'] = Movie_Data['runtime'].fillna(Movie_Data['runtime'].mean())
+    
     # handling the missing value in homepage
     Movie_Data['homepage'] = Movie_Data['homepage'].fillna(
         'http://www.' + Movie_Data['original_title'].str.replace(' ', '').str.lower() + '.com/')
     
     # handle release date colomn
     Movie_Data['release_date'] = pd.to_datetime(Movie_Data['release_date'])
-    Movie_Data['year'] = Movie_Data['release_date'].dt.year
-    Movie_Data['month'] = Movie_Data['release_date'].dt.month
-    Movie_Data['day'] = Movie_Data['release_date'].dt.day
+    Movie_Data['release_year'] = Movie_Data['release_date'].dt.year
+    Movie_Data['release_month'] = Movie_Data['release_date'].dt.month
+    Movie_Data['release_day'] = Movie_Data['release_date'].dt.day
     Movie_Data.drop(columns=['release_date'], inplace=True)
     Movie_Data.drop(columns=['overview'], inplace=True)
     
@@ -67,12 +78,12 @@ def Preprocessing(Movie_Data):
     Movie_Data = list_dict_encoding(Movie_Data, 'keywords', 'keywords_ids', 'keywords_name', "id", "name")
     
     one_counts = Movie_Data.iloc[:, 18:].sum()
-    cols_to_drop = one_counts[one_counts < 760].index
+    cols_to_drop = one_counts[one_counts < Movie_Data.shape[0]/4].index
     Movie_Data = Movie_Data.drop(cols_to_drop, axis=1)
     Movie_Data = pd.concat([Movie_Data, Y], axis=1, join="inner")
     
     corr = Movie_Data.corr()
-    top_feature = corr.index[abs(corr['vote_average']) > 0.15]
+    top_feature = corr.index[abs(corr['vote_average']) > 0.01]
     #Correlation plot
     plt.subplots(figsize=(12, 8))
     top_corr = Movie_Data[top_feature].corr()
@@ -81,5 +92,9 @@ def Preprocessing(Movie_Data):
     Movie_Data = Movie_Data[top_feature]
     top_feature = top_feature.delete(-1)
     print("Number of top features:", len(top_feature))
-    
+    #scaling features
+    X = Movie_Data[top_feature]
+    X = featureScaling(X, 0, 1)
+    X = pd.DataFrame(X,columns=top_feature)
+    Movie_Data = pd.concat([X, Y], axis=1, join="inner")
     return Movie_Data
